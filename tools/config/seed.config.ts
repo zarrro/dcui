b9ce5c6b058fd8e2de325bdeebe21807b3ea07d1
zarrro <spprint@gmail.com>
2016-12-22 00:46:55 +0200

    - gitignore npm logs, sublimetext project - package.json - update multiple packages - typescript update to 2.1.4, - fix consequences of the typescript update: drop the codelyzer tslint package, fix tslint rules, udpate typings - angular-protractor typings are dropped due to compatibility issues with the latest selenium webdriver typings - typings Q,jasmine,node are replaced, the types will come from @types node module

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -330,7 +330,6 @@ export class SeedConfig {
       '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js',
-
       '@angular/common/testing': 'node_modules/@angular/common/bundles/common-testing.umd.js',
       '@angular/compiler/testing': 'node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
       '@angular/core/testing': 'node_modules/@angular/core/bundles/core-testing.umd.js',
