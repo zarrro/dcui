508f23a8fc6b99abc4d6a3ca0ff85fbba0108869
zaro <spprint@gmail.com>
2017-03-26 21:52:03 +0300

    prod env api url config

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,8 +2,7 @@ import {EnvConfig} from './env-config.interface';
 
 const DevConfig: EnvConfig = {
   ENV: 'DEV',
-  // API: 'http://78.90.118.56:8080'
-  API: 'http://localhost:8080'
+  API: 'http://192.168.0.105:8080'
 };
 
 export = DevConfig;
--- tools/env/prod.ts
+++ tools/env/prod.ts
@@ -2,6 +2,7 @@ import {EnvConfig} from './env-config.interface';
 
 const ProdConfig: EnvConfig = {
   ENV: 'PROD'
+  API: 'http://www.dermal.clinic/api'
 };
 
 export = ProdConfig;
