ce4fa9ec2a108ad8f247943afb5748cb45556024
zaro <spprint@gmail.com>
2017-03-27 23:46:19 +0300

    make dev to use local reverse proxy for the backend

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,7 +2,7 @@ import {EnvConfig} from './env-config.interface';
 
 const DevConfig: EnvConfig = {
   ENV: 'DEV',
-  API: 'http://192.168.0.105:8080'
+  API: 'http://192.168.0.105/api'
 };
 
 export = DevConfig;
