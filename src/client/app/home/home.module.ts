17b183efd4979541773770cc3224cd7e28b64beb
zaro <spprint@gmail.com>
2017-05-16 23:15:46 +0300

    add  how it works home page componet

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -4,13 +4,14 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { IntroComponent } from './intro.component';
 import { SocialproofComponent } from './socialproof.component';
+import { HowitworksComponent } from './howitworks.component';
 import { FactsComponent } from './facts.component';
 import { TeamComponent } from './team.component';
 
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [HomeComponent, IntroComponent, SocialproofComponent, FactsComponent, TeamComponent],
+  declarations: [HomeComponent, IntroComponent, SocialproofComponent, FactsComponent, TeamComponent, HowitworksComponent],
   exports: [HomeComponent]
 })
 export class HomeModule { }
---
+++ src/client/app/home/howitworks.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded HomeComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'howitworks',
+  templateUrl: 'howitworks.component.html'
+})
+export class HowitworksComponent {
+
+}
