307596ecd2ea87896d9b06565cbee41a6dc9721c
zaro <spprint@gmail.com>
2017-05-21 13:57:55 +0300

    implement professionals section

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -7,11 +7,14 @@ import { SocialproofComponent } from './socialproof.component';
 import { HowitworksComponent } from './howitworks.component';
 import { FactsComponent } from './facts.component';
 import { TeamComponent } from './team.component';
+import { ProfessionalsComponent } from './professionals.component';
+
 
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [HomeComponent, IntroComponent, SocialproofComponent, FactsComponent, TeamComponent, HowitworksComponent],
+  declarations: [HomeComponent, IntroComponent, SocialproofComponent, FactsComponent,
+    TeamComponent, HowitworksComponent, ProfessionalsComponent],
   exports: [HomeComponent]
 })
 export class HomeModule { }
---
+++ src/client/app/home/professionals.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+/**
+ * This class represents the lazy loaded HomeComponent.
+ */
+@Component({
+  moduleId: module.id,
+  selector: 'prof',
+  templateUrl: 'professionals.component.html'
+})
+
+export class ProfessionalsComponent {
+
+}
